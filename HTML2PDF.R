bee6e2770c331fab97c59a9526889f3a99f9488b
Florian Privé <florian.prive.21@gmail.com>
2018-04-19 20:23:54 +0200

    typo

--- HTML2PDF.R
+++ HTML2PDF.R
@@ -4,6 +4,6 @@ decktape <- "decktape-1.0.0/"
 system(glue::glue(
   "{decktape}phantomjs {decktape}decktape.js", 
   " {basename}.html {basename}.pdf", 
-  " --load-pause 1000", 
+  " --load-pause 5000", 
   " -s 1504x1129"
 ))
