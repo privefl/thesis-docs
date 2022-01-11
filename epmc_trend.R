7426120a27bee32a94e3560f368c1f4224bb9e22
Florian Privé <florian.prive.21@gmail.com>
2022-01-11 09:54:14 +0100

    save for now

--- epmc_trend.R
+++ epmc_trend.R
@@ -2,8 +2,8 @@ library(europepmc)
 library(cowplot)
 library(tidyverse)
 
-ot_trend <- europepmc::epmc_hits_trend(query = "polygenic risk scores", 
-                                       period = 2000:2018)
+ot_trend <- europepmc::epmc_hits_trend(query = "polygenic scores", 
+                                       period = 2000:2021)
 # Standard plot
 
 ot_trend %>% 
@@ -17,8 +17,9 @@ ot_trend %>%
   ggplot(aes(x = factor(year), y = (query_hits / all_hits))) +
   geom_col(fill = "#56B4E9", width = 0.6, alpha = 0.9) +
   scale_y_continuous(expand = c(0, 0)) +
-  scale_x_discrete(breaks = seq(2000, 2018, by = 2)) +
+  scale_x_discrete(breaks = seq(2000, 2030, by = 2)) +
   theme_minimal_hgrid(12) +
   labs(x = "Year", y = "Proportion of all published articles") +
-  ggtitle("Interest in 'Polygenic Risk Scores' research since 2000",
-          "(query of 'polygenic risk scores' in PubMed Central)")
+  ggtitle("Interest in 'polygenic scores' research since 2000",
+          "(query of 'polygenic scores' in PubMed Central)")
+# ggsave("figures/PRS-trend22.png", width = 9, height = 6, scale = 0.75)
