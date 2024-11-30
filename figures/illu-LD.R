aa52b0f34be38959996b4b58cea10fafe61934f0
Florian Privé <florian.prive.21@gmail.com>
2024-11-30 06:36:09 +0100

    modify illu LD

--- figures/illu-LD.R
+++ figures/illu-LD.R
@@ -1,11 +1,11 @@
 corr <- readRDS(url("https://www.dropbox.com/s/65u96jf7y32j2mj/spMat.rds?raw=1"))
 corrT <- as(corr, "dgTMatrix")
-THR_R2 <- 0.02
+# THR_R2 <- 0.02
 ind <- 81:116
 library(magrittr)
 df2 <- corrT[ind, ind] %>%
   { dplyr::filter(data.frame(i = .@i + ind[1], j = .@j + ind[1], r2 = .@x^2),
-                  r2 >= THR_R2) }
+                  abs(i - j) <= 10) }
 
 geom_square <- function(min, max) {
   geom_segment(data = data.frame(i = c(min, min, max, max), 
@@ -19,7 +19,7 @@ library(ggplot2)
 ggplot(df2, aes(i, j)) +
   theme_minimal(16) +
   geom_raster(aes(fill = r2), alpha = 0.6) +
-  scale_fill_viridis_c(direction = -1, breaks = c(0.02, 0.1, 0.3, 0.5, 0.8)) +
+  scale_fill_viridis_c(direction = -1, breaks = c(0, 0.02, 0.1, 0.3, 0.5, 0.8, 1)) +
   coord_equal() +
   scale_x_continuous(breaks = 1:1000 - 0.5, minor_breaks = NULL) +
   scale_y_reverse(breaks = 1:1000 - 0.5, minor_breaks = NULL) +
